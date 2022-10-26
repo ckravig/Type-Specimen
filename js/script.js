a21151bffe77bfe921ecddd982a0411d4ca6733a
Collin Kravig <ckravig@gmail.com>
2022-10-26 12:28:52 -0500

    Start Characters and added JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+function changeWeight(value){
+    var element = document.getElementsByClassName("charactersText");
+    element.style.fontWeight = value;
+
+    alert(value);
+}
+
+function alertTest(value) {
+    alert(value);
+}
