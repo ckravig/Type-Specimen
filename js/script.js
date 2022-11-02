09d8f07eb2461632bfe755a62ce46f7dcc1fc45c
Collin Kravig <ckravig@gmail.com>
2022-11-01 23:33:49 -0500

    Added more JS and Body Text Samples

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Change Character section font weight
 function changeWeight(value){
     var characters = document.getElementById("ct");
     characters.style.fontWeight = value;
@@ -11,6 +11,29 @@ function changeWeight(value){
     ddCont.style.minWidth = "117px";
 }
 
-function alertTest(value) {
-    alert(value);
+// Change Character section font weight
+function changeSize(value){
+    var characters = document.getElementById("ct");
+    characters.style.fontSize = value;
+
+    var btn = document.getElementById("csBtn");
+    btn.textContent = "Size: " + value + "pt";
+
+    var ddCont = document.getElementById("csddCont");
+    ddCont.style.minWidth = "105px";
+}
+
+// Toggle character section font style | Italic || Normal |
+var currToggle = 0;
+function toggleItalic(){
+    var characters = document.getElementById("ct");
+
+    if (currToggle == 0) {
+        characters.style.fontStyle = "italic";
+        currToggle = 1;
+    }else{
+        characters.style.fontStyle = "normal";
+        currToggle = 0;
+    }
+    
 }
